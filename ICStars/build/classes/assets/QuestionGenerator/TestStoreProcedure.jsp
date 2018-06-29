697b653c0b56a315a7f6c1925308171fec3e875b
Idries Yaseen Kysia <idriesk@gmail.com>
2018-06-29 12:30:49 -0500

    The Big Test File Saving
    the basic setup of creating tests through xml and having html
    generation for questions so the tests can be done generated dynamically
    through html and text areas

---
+++ ICStars/build/classes/assets/QuestionGenerator/TestStoreProcedure.jsp
@@ -0,0 +1,19 @@
+<!--/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assets.QuestionGenerator;
+
+/**
+ *
+ * @author idrieskysia
+ */-->
+ 
+ <%
+package assets.QuestionGenerator;
+
+
+
+
+%>
--- ICStars/src/TestGenerator.java
+++ ICStars/src/TestGenerator.java
@@ -49,25 +49,11 @@ public class TestGenerator {
             String temp = "";
             String temp2 = "";
 
-
             boolean ans4 = true;
             boolean ans5 = true;
-//            boolean ans1 = (ans4 || (sign2 && v1==v2));
-//            boolean ans2 = (ans5 || (sign5 && v3==v4));
             boolean ans3;
 
-
-
-
-//            When.BooleanConditionBuilder hi = new When.BooleanConditionBuilder().
-
-
-//            int v1 = (int)(((ceil-floor)* sign.nextDouble()) + floor);
-//            int v2 = (int)(((ceil-floor)* sign.nextDouble()) + floor);
-//            int v3 = (int)(((ceil-floor)* sign.nextDouble()) + floor);
-//            int v4 = (int)(((ceil-floor)* sign.nextDouble()) + floor);
             temp = temp.concat("is " + v1 + " ");
-
             if(sign1) {
                 temp = temp.concat(">");
                 ans4 = v1 > v2;
@@ -83,8 +69,6 @@ public class TestGenerator {
                 temp = temp.concat(" ");
             }
             temp = temp.concat("" + v2);
-
-
 
             temp2 = temp2.concat("" + v3 + " ");
             if(sign4) {
@@ -107,7 +91,6 @@ public class TestGenerator {
             boolean ans1 = (ans4 || (sign2 && v1==v2));
             boolean ans2 = (ans5 || (sign5 && v3==v4));
 
-
             if(sign3)
             {
                 temp = temp.concat(" or ");
@@ -119,25 +102,7 @@ public class TestGenerator {
                 ans3 = ans1 && ans2;
 
             }
-
             temp = temp.concat(temp2);
-//            temp = temp.concat("" + v3 + " ");
-//            if(sign4) {
-//                temp = temp.concat(">");
-//            }
-//            else{
-//                temp = temp.concat("<");
-//            }
-//            if(sign5) {
-//                temp = temp.concat("= ");
-//            }
-//            else{
-//                temp = temp.concat(" ");
-//            }
-//            temp = temp.concat("" + v4);
-
-
-//            boolean answer = true;
 
             return new TestQuestion(++previousQNum,temp,ans3);
         }
@@ -147,134 +112,9 @@ public class TestGenerator {
             return new TestQuestion(1,"2");
     }
     
-//    public 
-//    public static class BooleanQuestion extends TestQuestion{
-//        public static BooleanQuestion Generate(int previousQNum, int floor, int ceil){
-//
-//            boolean sign1 = sign.nextBoolean();
-//            boolean sign2 = sign.nextBoolean();
-//            boolean sign3 = sign.nextBoolean();
-//            boolean sign4 = sign.nextBoolean();
-//            boolean sign5 = sign.nextBoolean();
-//
-//            int v1 = (int)(((ceil-floor)* sign.nextDouble()) + floor);
-//            int v2 = (int)(((ceil-floor)* sign.nextDouble()) + floor);
-//            int v3 = (int)(((ceil-floor)* sign.nextDouble()) + floor);
-//            int v4 = (int)(((ceil-floor)* sign.nextDouble()) + floor);
-//
-//            String temp = "";
-//            String temp2 = "";
-//
-//
-//            boolean ans4 = true;
-//            boolean ans5 = true;
-////            boolean ans1 = (ans4 || (sign2 && v1==v2));
-////            boolean ans2 = (ans5 || (sign5 && v3==v4));
-//            boolean ans3;
-//
-//
-//
-//
-////            When.BooleanConditionBuilder hi = new When.BooleanConditionBuilder().
-//
-//
-////            int v1 = (int)(((ceil-floor)* sign.nextDouble()) + floor);
-////            int v2 = (int)(((ceil-floor)* sign.nextDouble()) + floor);
-////            int v3 = (int)(((ceil-floor)* sign.nextDouble()) + floor);
-////            int v4 = (int)(((ceil-floor)* sign.nextDouble()) + floor);
-//            temp = temp.concat("is " + v1 + " ");
-//
-//            if(sign1) {
-//                temp = temp.concat(">");
-//                ans4 = v1 > v2;
-//            }
-//            else{
-//                temp = temp.concat("<");
-//                ans4 = v1 < v2;
-//            }
-//            if(sign2) {
-//                temp = temp.concat("= ");
-//            }
-//            else{
-//                temp = temp.concat(" ");
-//            }
-//            temp = temp.concat("" + v2);
-//
-//
-//
-//            temp2 = temp2.concat("" + v3 + " ");
-//            if(sign4) {
-//                temp2 = temp2.concat(">");
-//                ans5 = v3 > v4;
-//            }
-//            else{
-//                temp2 = temp2.concat("<");
-//                ans5 = v3 < v4;
-//
-//            }
-//            if(sign5) {
-//                temp2 = temp2.concat("= ");
-//            }
-//            else{
-//                temp2 = temp2.concat(" ");
-//            }
-//            temp2 = temp2.concat("" + v4);
-//
-//            boolean ans1 = (ans4 || (sign2 && v1==v2));
-//            boolean ans2 = (ans5 || (sign5 && v3==v4));
-//
-//
-//            if(sign3)
-//            {
-//                temp = temp.concat(" or ");
-//                ans3 = ans1 || ans2;
-//            }
-//            else
-//            {
-//                temp = temp.concat(" and ");
-//                ans3 = ans1 && ans2;
-//
-//            }
-//
-//            temp = temp.concat(temp2);
-////            temp = temp.concat("" + v3 + " ");
-////            if(sign4) {
-////                temp = temp.concat(">");
-////            }
-////            else{
-////                temp = temp.concat("<");
-////            }
-////            if(sign5) {
-////                temp = temp.concat("= ");
-////            }
-////            else{
-////                temp = temp.concat(" ");
-////            }
-////            temp = temp.concat("" + v4);
-//
-//
-////            boolean answer = true;
-//
-//            return new TestQuestion(++previousQNumber ,temp,ans3);
-//        }
-//
-//        public BooleanQuestion(int n, String q, boolean ans) {
-//            super(n, q, ans);
-//        }
-//    }
 
 
     public static void main(String[] args){
-//        TestQuestion t1 = TestGenerator.Generate(0,10,55);
-//        TestQuestion worstcase = TestGenerator.Generate(0,10,55);
-//
-////        int num = t1.QNumber;
-////        String out = t1.QQuestion;
-//
-//        System.out.println(t1.QNumber + ": "+t1.QQuestion + " is " + t1.QAnswer);
-//        for (int i  = 0; i<1000; i++){
-//            t1 = TestGenerator.Generate(t1.QNumber,10,55);
-//            System.out.println(t1.QNumber + ": "+t1.QQuestion + " is " + t1.QAnswer);
-//        }
+
     }
 }
